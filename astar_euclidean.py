4b039bc28d5d406c37d1d14ed69a0d91d83df43d
DannielK <dannykm003@gmail.com>
2024-05-05 06:48:27 -0700

    Added comments to euclidean, added output data for no solution

--- astar_euclidean.py
+++ astar_euclidean.py
@@ -18,7 +18,9 @@ def find_tile_goal_position(node: Problem, value):
 
 # calculate the euclidean heuristic of a current state
 def heuristic(node: Problem):
+    # heuristic value
     h_n = 0
+    # cost to state
     g_n = node.g_n
     for i, row in enumerate(node.initial_state):
         for j, element in enumerate(row):
@@ -26,26 +28,31 @@ def heuristic(node: Problem):
                 continue
             goal_row, goal_col = find_tile_goal_position(node, element)
             h_n += euclidean_distance((i,j), (goal_row, goal_col))
+    # cost to goal from state
     f_n = g_n + h_n
     return g_n, h_n, f_n
 
+# find the position of the blank tile
 def find_blank_tile(node: Problem):
     for i, row in enumerate(node.initial_state):
         for j, element in enumerate(row):
             if element == 0:
                 return i, j
 
+# algorithm for A* using euclidean distances as the heuristics
 def astar_euclidean(initial: Problem):
-    # initialize the problem by adding root node to frontier
+    # initialize the problem by adding initial node to frontier
     initial.g_n, initial.h_n, initial.f_n = heuristic(initial)
     frontier = [initial] 
     # set of all expanded nodes
     expanded_set = {}
+    # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
     dupe_state = ""
     for i, row in enumerate(initial.initial_state):
         for j, element in enumerate(row):
             dupe_state += str(element)
-    dupe_frontier_states = {dupe_state: 0}
+    # list of strictly states in frontier
+    frontier_states = {dupe_state: 0}
     # max length of queue at any time
     max_queue = 0
     # depth of goal node
@@ -58,11 +65,13 @@ def astar_euclidean(initial: Problem):
 
         # choose a leaf node and remove it from frontier
         node = copy.deepcopy(heapq.heappop(frontier))
+        # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
         dupe_state = ""
         for i, row in enumerate(node.initial_state):
             for j, element in enumerate(row):
                 dupe_state += str(element)
-        del dupe_frontier_states[dupe_state]
+        # also remove the node from the frontier states
+        del frontier_states[dupe_state]
 
         # if node contains goal state then return corresponding solution
         if node.is_done:
@@ -75,54 +84,62 @@ def astar_euclidean(initial: Problem):
 
         # expand chosen node, adding resulting node to frontier
         print("The best state to expand with g(n) = " + str(node.g_n) + " and h(n) = " + str(node.h_n) + " is...\n" + str(node))
-
+        # position of blank tile at current state
         x_blank, y_blank = find_blank_tile(node)
+        # attempt move up operation, if so and not existing state, then add node to frontier
         try:
             move_up = node.move_tile_up(x_blank + 1, y_blank)
             move_up.g_n, move_up.h_n, move_up.f_n = heuristic(move_up)
+            # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
             dupe_state = ""
             for i, row in enumerate(move_up.initial_state):
                 for j, element in enumerate(row):
                     dupe_state += str(element)
-            if (dupe_state not in dupe_frontier_states) and (dupe_state not in expanded_set):
+            if (dupe_state not in frontier_states) and (dupe_state not in expanded_set):
                 heapq.heappush(frontier, move_up)
-                dupe_frontier_states[dupe_state] = 0
+                frontier_states[dupe_state] = 0
         except:
             pass
+        # attempt move down operation, if so and not existing state, then add node to frontier
         try:
             move_down = node.move_tile_down(x_blank - 1, y_blank)
             move_down.g_n, move_down.h_n, move_down.f_n = heuristic(move_down)
+            # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
             dupe_state = ""
             for i, row in enumerate(move_down.initial_state):
                 for j, element in enumerate(row):
                     dupe_state += str(element)
-            if (dupe_state not in dupe_frontier_states) and (dupe_state not in expanded_set):
+            if (dupe_state not in frontier_states) and (dupe_state not in expanded_set):
                 heapq.heappush(frontier, move_down)
-                dupe_frontier_states[dupe_state] = 0            
+                frontier_states[dupe_state] = 0            
         except:
             pass
+        # attempt move left operation, if so and not existing state, then add node to frontier
         try:
             move_left = node.move_tile_left(x_blank, y_blank + 1)
             move_left.g_n, move_left.h_n, move_left.f_n = heuristic(move_left)
+            # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
             dupe_state = ""
             for i, row in enumerate(move_left.initial_state):
                 for j, element in enumerate(row):
                     dupe_state += str(element)
-            if (dupe_state not in dupe_frontier_states) and (dupe_state not in expanded_set):
+            if (dupe_state not in frontier_states) and (dupe_state not in expanded_set):
                 heapq.heappush(frontier, move_left)
-                dupe_frontier_states[dupe_state] = 0
+                frontier_states[dupe_state] = 0
         except:
             pass
+        # attempt move right operation, if so and not existing state, then add node to frontier
         try:
             move_right = node.move_tile_right(x_blank, y_blank - 1)
             move_right.g_n, move_right.h_n, move_right.f_n = heuristic(move_right)
+            # this changes the matrix to a 9 digit string for using as keys in dictionaries to optimize lookups
             dupe_state = ""
             for i, row in enumerate(move_right.initial_state):
                 for j, element in enumerate(row):
                     dupe_state += str(element)
-            if (dupe_state not in dupe_frontier_states) and (dupe_state not in expanded_set):
+            if (dupe_state not in frontier_states) and (dupe_state not in expanded_set):
                 heapq.heappush(frontier, move_right)
-                dupe_frontier_states[dupe_state] = 0
+                frontier_states[dupe_state] = 0
         except:
             pass
 
